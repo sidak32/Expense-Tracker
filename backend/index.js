09e815c4412366c19478e3caba7fe2f17e3a2daf
sidak32 <sidak2k4@gmail.com>
2025-07-01 09:00:43 +0530

    User & Expense model

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./database/db.js";
 
 dotenv.config({}); //environment variables detect
 
@@ -10,14 +11,15 @@ const PORT = 8000;
 
 //middleware
 app.use(express.json());
-app.use(urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin: "http://localhost:5173", //React Vite port no
-    credentials: true
-}
+  origin: "http://localhost:5173", //React Vite port no
+  credentials: true,
+};
 app.use(cors(corsOptions));
 
 app.listen(PORT, () => {
-    console.log(`Server listen at ${PORT}`);
-})
+  connectDB();
+  console.log(`Server listen at ${PORT}`);
+});
